6386b4a6ef231e5e0f974441d85a447e583231eb
Mark Ellzey <ellzey@censys.io>
2023-10-18 20:47:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Censys import-host Git Plugin
+
+This is a simple script that will import a host's history from [Censys](https://search.censys.io) into a Git repository,
+allowing for analysts to quickly review the history of a host in a git-intuitive way.
+
+This script attempts to preserve the timestamp of the host change into the commit log, and logs every major action as the commit message.
+
+### Install
+
+1. `pip install -r requirements.txt`
+2. Copy `git-import-host.py` to `$PATH/git-import-host` and `chmod +x` to make it executable.
+
+### Run
+
+1. `mkdir host_history/ && cd host_history/`
+2. Initialize the repository: `git import-host --init`
+3. Import the host `1.1.1.1`'s history between 2023-10-01 and 2023-10-03: `git import-host -f 2023-10-01 -t 2023-10-03 1.1.1.1`
+4. Profit?
+
+```
+lz@localhost:~/host_history$ git import-host -f 2023-10-01 -t 2023-10-03 1.1.1.1 
+lz@localhost:~/host_history$ git log --reverse -5 -p 1.1.1.1
+commit 9266a5875da425df0f17b1eb91b9d8d3ba51af3d
+Author: Mark Ellzey <ellzey@censys.io>
+Date:   Tue Oct 3 20:37:34 2023 -0400
+
+    replace /services/15/banner
+
+diff --git a/1.1.1.1 b/1.1.1.1
+index a0ed4c0..4f75c93 100644
+--- a/1.1.1.1
++++ b/1.1.1.1
+@@ -1660,8 +1660,8 @@
+         },
+         "supports_http2": false
+       },
+-      "observed_at": "2023-10-02T21:49:44.925170113Z",
+-      "perspective_id": "PERSPECTIVE_TELIA",
++      "observed_at": "2023-10-03T21:16:46.292795981Z",
++      "perspective_id": "PERSPECTIVE_TATA",
+       "port": 8080,
+       "service_name": "HTTP",
+       "software": [
+@@ -1701,10 +1701,10 @@
+         "fingerprint": "27d3ed3ed0003ed1dc42d43d00041d6183ff1bfae51ebd88d70384363d525c",
+         "cipher_and_version_fingerprint": "27d3ed3ed0003ed1dc42d43d00041d",
+         "tls_extensions_sha256": "6183ff1bfae51ebd88d70384363d525c",
+-        "observed_at": "2023-09-15T23:58:14.884313902Z"
++        "observed_at": "2023-10-03T23:04:48.296685713Z"
+       },
+-      "observed_at": "2023-10-02T03:04:25.096217513Z",
+-      "perspective_id": "PERSPECTIVE_ORANGE",
++      "observed_at": "2023-10-03T20:16:31.108109682Z",
++      "perspective_id": "PERSPECTIVE_TATA",
+       "port": 8443,
+       "service_name": "UNKNOWN",
+       "software": [
+@@ -1715,7 +1715,7 @@
+           "source": "OSI_TRANSPORT_LAYER"
+         }
+       ],
+-      "source_ip": "167.94.145.59",
++      "source_ip": "167.94.138.36",
+       "tls": {
+         "version_selected": "TLSv1_3",
+         "cipher_selected": "TLS_CHACHA20_POLY1305_SHA256",
+@@ -1828,7 +1828,7 @@
+         "banner": "DISPLAY_UTF8",
+         "banner_hex": "DISPLAY_HEX"
+       },
+-      "banner": "HTTP/1.1 301 Moved Permanently\r\nServer: cloudflare\r\nDate:  <REDACTED>\r\nContent-Type: text/html\r\nContent-Length: 167\r\nConnection: keep-alive\r\nLocation: https://1.1.1.1/\r\nCF-RAY: 80f71f64e90f2300-ORD\r\n",
++      "banner": "HTTP/1.1 301 Moved Permanently\r\nServer: cloudflare\r\nDate:  <REDACTED>\r\nContent-Type: text/html\r\nContent-Length: 167\r\nConnection: keep-alive\r\nLocation: https://1.1.1.1/\r\nCF-RAY: 81083a5118f622f7-ORD\r\n",
+       "banner_hashes": [
+         "sha256:bc0d1d97f59364b0b8fd4bdaf5f864bb04c4bc891b8eb814d002df1316907e18"
+       ],
+```
---
+++ git-import-host.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+import argparse
+import censys.search
+import jsonpatch
+import json
+from datetime import date, timedelta
+import git
+
+class CensysEntry:
+    def __init__(self, host, at_time, censys_ctx) -> None:
+        self.host = host
+        self.at_time = at_time
+        self.to_time = at_time + timedelta(days=1)
+        self.censys_ctx = censys_ctx
+        self.data = self.censys_ctx.view(self.host, at_time=self.at_time)
+        self.diff = self.censys_ctx.view_host_diff(
+            self.host, at_time=self.at_time, at_time_b=self.to_time)
+        self.patches = self.diff["patch"]
+
+    def apply_patch(self):
+        return jsonpatch.apply_patch(self.data, self.patches)
+
+
+class CensysManager:
+    def __init__(self, host, start_date, end_date, repo='./'):
+
+        self.host = host
+        self.start_date = start_date
+        self.end_date = end_date
+        self.repo = repo
+        self.entries = self._generate_entries()
+
+    def _generate_entries(self):
+        h = censys.search.CensysHosts()
+        entries = []
+        current_date = self.start_date
+        while current_date <= self.end_date:
+            entries.append(CensysEntry(self.host, current_date, h))
+            current_date += timedelta(days=1)
+        return entries
+
+    def _is_operation_filtered(self, patch):
+        # for a clean commit history, we will take these fields into account,
+        # but not actually commit each one.
+        filtered = {
+            "replace": [
+                "source_ip",
+                "perspective_id",
+                "observed_at",
+                "last_updated_at",
+            ]
+        }
+        if patch["op"] in filtered:
+            for path in filtered[patch["op"]]:
+                if path in patch["path"]:
+                    return True
+
+        return False
+    def save_to_repo(self, use_branch=""):
+        repository = None
+        repository = git.Repo(self.repo)
+
+        if use_branch != "":
+            branch = repository.create_head(use_branch)
+            repository.head.reference = branch
+
+        first_data = self.entries[0].data
+        filename = f"{self.repo}/{self.host}"
+
+        with open(filename, 'w+', newline='\n') as f:
+            f.write(json.dumps(first_data, indent=2))
+            repository.index.add([f'{self.host}'])
+            repository.index.commit(f"Update {self.host}")
+
+        is_dangling = False
+        for entry in self.entries:
+            for patch in entry.patches:
+                first_data = jsonpatch.apply_patch(first_data, [patch])
+                with open(filename, 'w', newline='\n') as f:
+                    f.write(json.dumps(first_data, indent=2))
+                    f.flush()
+
+                if self._is_operation_filtered(patch):
+                    is_dangling = True
+                    continue
+
+                is_dangling = False
+                repository.git.commit('--date', entry.at_time, '-a',
+                                      '-m', f'{patch["op"]} {patch["path"]}')
+        if is_dangling:
+            # this is a dumb way to handle this.
+            repository.git.commit('-a', '-m', 'final')
+
+def _is_censys_repo(repo):
+    try:
+        with open(f"{repo}/.censys", 'r') as f:
+            return True
+    except FileNotFoundError:
+        return False
+
+def _needs_init(repo):
+    try:
+        _ = git.Repo(repo).git_dir
+        return False
+    except git.exc.InvalidGitRepositoryError:
+        return True
+
+def main(args):
+    if args.host == "init" and _needs_init(args.repo):
+        print("Initializing new Censys host import repository.")
+
+        with open(f"{args.repo}/.censys", 'w+') as f:
+            f.write("This is a Censys host import repository.")
+
+        repo = git.Repo.init(args.repo)
+        repo.index.add([".censys"])
+        repo.git.commit('-a', '-m', 'Initial commit')
+        print("Initialized new Censys host import repository.")
+        exit(0)
+
+    if not _is_censys_repo(args.repo):
+        print("Error: not a censys host import repository (use 'init' to create one).")
+        exit(1)
+
+    if args.from_date is None or args.to_date is None:
+        print("Error: --from-date and --to-date are required")
+        exit(1)
+
+    manager = CensysManager(args.host, args.from_date, args.to_date, repo=args.repo)
+    manager.save_to_repo(use_branch=args.branch)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process Censys data and save to git repo.')
+    parser.add_argument('host', help='Host IP to process')
+    parser.add_argument('-f', '--from-date', type=lambda s: date.fromisoformat(s), help='Start date in the format YYYY-MM-DD')
+    parser.add_argument('-t', '--to-date', type=lambda s: date.fromisoformat(s), help='End date in the format YYYY-MM-DD')
+    parser.add_argument('-r', '--repo', default='.', help='Path to git repo')
+    parser.add_argument('-b', '--branch', default="", help="Create a new branch and import the host.")
+    args = parser.parse_args()
+
+    main(args)
+
